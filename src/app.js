afd959f56c11bc2a33b227d979d60c12b29d186c
UmaVenkatesh_Andavar <UmaVenkatesh.Andavarapu@dellteam.com>
2025-05-20 08:12:34 +0530

    dynamic signUp

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ const {connectDB} = require("./config/database")
 const app = express();
 const userModel = require("./models/user")
 
+app.use(express.json())
+
 app.post("/signUp", async (req,res)=>{
-    const userObj = {
-        firstName : "Jasprit",
-        lastName : "Bumrah",
-        emailId : "jb93@gmail.com",
-        password : "Sanjana@143"
-    }
-    const user = new userModel(userObj);
+    // const userObj = {
+    //     firstName : "Jasprit",
+    //     lastName : "Bumrah",
+    //     emailId : "jb93@gmail.com",
+    //     password : "Sanjana@143"
+    // }
+    console.log(req.body);
+    
+    const user = new userModel(req.body);
    try{
         await user.save();
         res.send("used added successfully")
